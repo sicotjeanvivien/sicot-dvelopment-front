c69f8945b0f6b6b8d52af153d024c1cc42eded5d
jean viven <jeanvivien.sicot@lapiscine.pro>
2022-02-07 09:02:55 +0100

    init project

---
+++ src/Components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../Assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
